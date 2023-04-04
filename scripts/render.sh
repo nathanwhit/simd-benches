5cbe24390b28548fa1f5ad028a6df546e23eec53
Nugine <nugine@foxmail.com>
2023-04-05 01:39:43 +0800

    render

--- scripts/render.sh
+++ scripts/render.sh
@@ -3,32 +3,26 @@
 # shellcheck disable=SC2012
 benches=$(ls benches | sed -e 's/\.rs$//')
 
-for dispatch in dynamic static-unstable fallback
-do
-    for bench in $benches
-    do
-        cat "$dispatch-$bench.jsonl" >> "$dispatch.jsonl"
-    done
-    python3 ./scripts/analyze.py "$dispatch.jsonl" > "$dispatch.md"
-done
-
-echo > result.md
-
 echo "# Benchmark Results" > result.md
 date -u --rfc-3339=s >> "result.md"
 
-for dispatch in dynamic static-unstable fallback
+for bench in $benches
 do
-    {
-        echo "## $dispatch"
-        echo
-        cat "$dispatch.md" 
-    } >> result.md
+    echo "## $bench" >> result.md
+    echo >> result.md
+    for dispatch in dynamic static-unstable fallback
+    do
+        {
+            echo "### $dispatch"
+            echo
+            python3 ./scripts/analyze.py "$dispatch-$bench.jsonl"
+        } >> result.md
+    done
 done
 
 echo "## Environment" >> result.md
 ./scripts/print-env.sh >> result.md
 
 mkdir dist
-grip result.md --export dist/index.html --user-content --wide --title simd-benches
+grip result.md --export dist/index.html --user-content --wide --title simd-benches --context Nugine/simd-benches
 ./scripts/hack.py dist/index.html
